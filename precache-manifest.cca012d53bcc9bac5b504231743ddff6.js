92e5fb2124a5cae834365ecc1eda742ceefe17fa
samjones310 <samjones31098@gmail.com>
2020-07-05 23:10:08 +0530

    Updates

---
+++ precache-manifest.cca012d53bcc9bac5b504231743ddff6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20877b54648342918dc234e6035230da",
+    "url": "/Kiran_project/index.html"
+  },
+  {
+    "revision": "f907477992b2488800f2",
+    "url": "/Kiran_project/static/css/main.de68544a.chunk.css"
+  },
+  {
+    "revision": "851a8a281da751059c5d",
+    "url": "/Kiran_project/static/js/2.da641949.chunk.js"
+  },
+  {
+    "revision": "729937898630e839994162d5d9f8ce89",
+    "url": "/Kiran_project/static/js/2.da641949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f907477992b2488800f2",
+    "url": "/Kiran_project/static/js/main.a2a3573d.chunk.js"
+  },
+  {
+    "revision": "967cc2b0d02bccad8753",
+    "url": "/Kiran_project/static/js/runtime-main.33ab33e3.js"
+  }
+]);
